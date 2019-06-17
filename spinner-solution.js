737deb400d66b66e01ee7870791589b126fd605d
Magesh <tkmagesh77@gmail.com>
2019-06-17 12:21:33 +0530

    Salary Calculator - initial solution

---
+++ spinner-solution.js
@@ -0,0 +1,28 @@
+var spinner = (function(){
+	var counter = 0;
+	return {
+		up : function(){
+			return ++counter;
+		},
+		down : function(){
+			return --counter;
+		}
+	};
+})();
+
+//methods - up, down
+
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+spinner.up() //=> 4
+
+spinner.down() //=> 3
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+
+spinner.counter = 1000
+spinner.up()
