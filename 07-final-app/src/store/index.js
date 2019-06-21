c8115224dd4d38d81f7bb38c255d0247f924d02b
Magesh <tkmagesh77@gmail.com>
2019-06-21 13:16:41 +0530

    introducing middlewares

--- 07-final-app/src/store/index.js
+++ 07-final-app/src/store/index.js
@@ -1,6 +1,27 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers';
 
-let appStore = createStore(rootReducer);
+
+/*function loggerMiddleware({ getState, dispatch}){
+	return function (next){
+		return function(action){
+			console.group(action.type);
+			console.log('before -> ', getState());
+			next(action);
+			console.log('after -> ', getState());
+			console.groupEnd();
+		}
+	}
+}*/
+
+let loggerMiddleware = ({getState, dispatch}) => next => action => {
+	console.group(action.type);
+	console.log('before -> ', getState());
+	next(action);
+	console.log('after -> ', getState());
+	console.groupEnd();
+}
+
+let appStore = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default appStore;
