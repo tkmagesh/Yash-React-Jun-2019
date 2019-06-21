5ba2398543d22b4f0d4555996b26a75777881b72
Magesh <tkmagesh77@gmail.com>
2019-06-21 15:30:22 +0530

    async using promises & async-await

--- 08-async/async-demos.js
+++ 08-async/async-demos.js
@@ -33,4 +33,43 @@
 
 	window['addAsyncClient'] = addAsyncClient;	
 
+	function addAsyncPromise(x,y){
+		console.log(`	[@Service] processing ${x} and ${y}`);
+		var p = new Promise(function(resolveFn, rejectFn){
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`	[@Service] returning result`);
+				resolveFn(result);
+			}, 4000);
+		});
+		return p;
+	}
+
+
+	function addAsyncPromiseClient(x,y){
+		/*
+		console.log(`[@Client] triggering the service`);
+		var result = addSync(x,y);
+		console.log(`[@Client] result = ${result}`);
+		*/
+
+		console.log(`[@Client] triggering the service`);
+		var promise = addAsyncPromise(x,y);
+		promise.then(function(result){
+			console.log(`[@Client] result = ${result}`);	
+		});
+
+	}
+
+	window['addAsyncPromiseClient'] = addAsyncPromiseClient;
+
+	async function addNewAsyncPromiseClient(x,y){
+		console.log(`[@Client] triggering the service`);
+		var result = await addAsyncPromise(x,y);
+		console.log(`[@Client] result = ${result}`);
+		return result * 2;
+	}
+
+	window['addNewAsyncPromiseClient'] = addNewAsyncPromiseClient;
+	
 })();
